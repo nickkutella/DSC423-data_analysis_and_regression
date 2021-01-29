71ec82721c45de16cac865c9f76d94f7abcd44e1
nickkutella <78060327+nickkutella@users.noreply.github.com>
2021-01-28 19:03:52 -0600

    Update dataVisualization.R
    Added random sample code and plot for ADR  vs season.

--- dataVisualization.R
+++ dataVisualization.R
@@ -1,5 +1,10 @@
 ### Paste plots and data visualzations here ###
 
+#Randomly take 1000 samples of the hotel booking dataset to streamline ploting function
+hotel_bookings_sample <- hotel_bookings[sample(1:nrow(hotel_bookings), 1000, replace=FALSE),]
+
+#Plot of Hotel Average Daily Rate by Season
+plot(hotel_bookings_sample$season, hotel_bookings_sample$adr, main="Hotel Bookings ADR by Season", xlab="Season", ylab="ADR")
 
 
 # Separate blocks for easy reading and please ensure you comment your code
